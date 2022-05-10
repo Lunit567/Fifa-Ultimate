2918e5cd5ec49eb8d19e7fff445c452e75d73ee9
Lunit567 <martin.lun.567@gmail.com>
2022-05-10 14:09:39 -0500

    git commit -m "Actualizacion"

--- app/src/main/java/luna/martin/fifa_ultimate/adaptador/FutbolistaAdapter.kt
+++ app/src/main/java/luna/martin/fifa_ultimate/adaptador/FutbolistaAdapter.kt
@@ -1,4 +1,42 @@
 package luna.martin.fifa_ultimate.adaptador
 
-class FutbolistaAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import luna.martin.fifa_ultimate.R
+import luna.martin.fifa_ultimate.modelo.Futbolista
+
+class FutbolistaAdapter(val listaFutbolistas:ArrayList<Futbolista>): RecyclerView.Adapter<FutbolistaAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder{
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.vista,parent,false)
+        return ViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.Nombre.text=listaFutbolistas[position].nombre
+        holder.ID.text=listaFutbolistas[position].id
+        holder.Posicion.text=listaFutbolistas[position].posicion
+        holder.Raro.text=listaFutbolistas[position].raro
+    }
+
+    override fun getItemCount(): Int {
+        return listaFutbolistas.size
+    }
+
+    class ViewHolder(vista: View):RecyclerView.ViewHolder(vista){
+        val Nombre: TextView
+        val ID: TextView
+        val Posicion: TextView
+        val Raro: TextView
+
+        init {
+            Nombre = vista.findViewById(R.id.NomFut)
+            ID = vista.findViewById(R.id.IDFut)
+            Posicion = vista.findViewById(R.id.PodiFut)
+            Raro = vista.findViewById(R.id.RaroFut)
+        }
+    }
+
 }
